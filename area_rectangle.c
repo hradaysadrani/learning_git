1f5d64b66558e58bad221930d44d149dde678ad2
Hraday <hradaysadrani@gmail.com>
2022-10-27 10:54:14 +0530

    First the 1st commit

---
+++ area_rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+       int length, breadth;
+       printf(" Enter length of rectangle\n ");
+       scanf("%d", &length);
+
+       printf("Enter breadth of rectangle\n");
+       scanf("%d", &breadth);
+
+       printf("The area of rectangle is %d", length * breadth);
+
+
+    return 0;
+}
